97206871c811a8805817cd0ef2438d72fe07c6cd
FilipiNyetz <romao.martins.filipi@gmail.com>
2025-04-17 19:45:25 -0300

    criacao da tela de perfil, adicao prettier e criacao componente de ingresso

---
+++ src/app/perfil/page.tsx
@@ -0,0 +1,19 @@
+import { Ingresso } from "@/components/ingresso";
+import { Card, CardContent } from "@/components/ui/card";
+
+export default function () {
+    return (
+        <main className="h-auto bg-background px-4 py-8 flex flex-col">
+            <div>
+                <h1 className="pl-8 text-3xl">
+                    Olá, <span>nome</span>
+                </h1>
+            </div>
+            <div className="w-full flex items-center flex-col">
+                <h1 className="text-xl mb-8">Seus <span className="text-primary-darker">ingressos</span>:</h1>
+                <Ingresso titulo="Festa Junina" data="Dia 30 | Mai." />
+                <Ingresso titulo="Calourada" expirado />
+            </div>
+        </main>
+    );
+}
---
+++ src/components/ingresso/index.tsx
@@ -0,0 +1,29 @@
+import { Card, CardContent } from "../ui/card";
+
+type IngressoProps = {
+    titulo: string;
+    data?: string;
+    expirado?: boolean;
+};
+
+export function Ingresso({ titulo, data, expirado = false }: IngressoProps) {
+    return (
+        <Card
+            className={`mb-3 md:w-90 flex ${!expirado ? "bg-background-light-yellow" : ''}`}
+            variant={expirado ? "expired" : "default"}
+        >
+            <CardContent className="flex items-center">
+                <div className="w-25">
+                    <h1 className="text-md">{titulo}</h1>
+                </div>
+                <Card variant={expirado ? "expired" : "default"} className="border-0">
+                    <CardContent className="w-30 md:w-40">
+                        <h3 className="text-xs w-full text-center md:text-md">
+                            {expirado ? "Expirado" : data}
+                        </h3>
+                    </CardContent>
+                </Card>
+            </CardContent>
+        </Card>
+    );
+}
